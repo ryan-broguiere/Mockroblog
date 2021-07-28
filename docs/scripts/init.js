1df7e3f5e598280df8a3808acc4632bd6467448d
ChristopherMillones <53508840+ChristopherMillones@users.noreply.github.com>
2021-07-27 18:25:51 -0700

    Added header and footer

--- docs/scripts/init.js
+++ docs/scripts/init.js
@@ -3,6 +3,26 @@ window.mockroblog = mockroblog
 
 const timeline = mockroblog.getPublicTimeline()
 
-const display = document.querySelector('#timeline-json')
-display.textContent = JSON.stringify(timeline, null, 2)
+// const display = document.querySelector('#timeline-json')
+// display.textContent = JSON.stringify(timeline, null, 2)
+
+
+/*
+Initialize all components from HTML
+*/
+
+const btn = document.querySelector(".mobile-menu-button")
+const body = document.querySelector(".main-screen")
+const menu = document.querySelector(".mobile-menu")
+
+//Event Listeners 
+
+btn.addEventListener('click', () =>{
+    menu.classList.toggle("hidden")
+})
+
+body.addEventListener('click', () => {
+    console.log("Clicked");
+    menu.classList.add("hidden")
+})
 
