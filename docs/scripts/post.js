5b432e9675669bbbdf971a6de09ff22e46e2c17b
ChristopherMillones <53508840+ChristopherMillones@users.noreply.github.com>
2021-07-29 10:50:27 -0700

    Post page is responsinve with api

--- docs/scripts/post.js
+++ docs/scripts/post.js
@@ -6,8 +6,10 @@ Initialize all components from HTML
 const btn = document.querySelector(".mobile-menu-button")
 const menu = document.querySelector(".mobile-menu")
 const textPost = document.querySelector("#text-post")
+const textDisplayPost = document.querySelector(".text-display-post")
 const btnPost = document.querySelector(".btn-post")
 
+let tempUser
 
 //Event Listeners 
 btn.addEventListener('click', () => {
@@ -15,5 +17,7 @@ btn.addEventListener('click', () => {
 })
 
 btnPost.addEventListener('click', () => {
-    console.log(textPost.value);
+    tempUser = mockroblog.postMessage(4, textPost.value)
+    textDisplayPost.textContent = tempUser.text
 })
+
