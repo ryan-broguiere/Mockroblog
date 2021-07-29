bf70810aa50d3c5cc81fe572228b533ae3041e7f
ChristopherMillones <53508840+ChristopherMillones@users.noreply.github.com>
2021-07-28 17:50:58 -0700

    Made some example js code for us to use

--- docs/scripts/aboutus.js
+++ docs/scripts/aboutus.js
@@ -1,12 +1,35 @@
+import * as mockroblog from './mockroblog.js'
 
 /*
 Initialize all components from HTML
 */
 const btn = document.querySelector(".mobile-menu-button")
 const menu = document.querySelector(".mobile-menu")
+const textPost = document.querySelector(".post")
+const textDate = document.querySelector(".date")
 //Event Listeners 
 
 btn.addEventListener('click', () => {
     console.log("Hello");
     menu.classList.toggle("hidden")
+})
+
+/*
+Access the mockroblog.js functions and store them into variables for easier access
+*/
+const timeline = mockroblog.getPublicTimeline() // function retruns array of objects
+
+// Example of accessing properties from objects 
+console.log(timeline[0].id, timeline[0].user_id, timeline[0].text, timeline[0].timestamp);
+
+// For Each example
+timeline.forEach(obj => {
+    console.log(obj.id);
+})
+
+// Example of displaying contents on HTML page
+timeline.forEach(obj => {
+    console.log(obj.id, obj.user_id, obj.text, obj.timestamp);
+    textDate.textContent = obj.timestamp
+    textPost.textContent = obj.text
 })
--- docs/scripts/init.js
+++ docs/scripts/init.js
@@ -1,10 +1,11 @@
 import * as mockroblog from './mockroblog.js'
 // window.mockroblog = mockroblog
 
-// const timeline = mockroblog.getPublicTimeline()
+const timeline = mockroblog.getPublicTimeline()
 
-// const display = document.querySelector('#timeline-json')
-// display.textContent = JSON.stringify(timeline, null, 2)
+console.log(typeof timeline);
+console.log(timeline);
+
 
 
 /*
