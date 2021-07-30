db87fa3004711f061cc7ff45ec36c13444d4eab2
ChristopherMillones <53508840+ChristopherMillones@users.noreply.github.com>
2021-07-29 17:34:36 -0700

    Added some small changes

--- docs/scripts/aboutus.js
+++ docs/scripts/aboutus.js
@@ -5,12 +5,23 @@ Initialize all components from HTML
 */
 const btn = document.querySelector(".mobile-menu-button")
 const menu = document.querySelector(".mobile-menu")
-const textPost = document.querySelector(".post")
-const textDate = document.querySelector(".date")
+const textPost = document.querySelector(".my-posts")
 
-
+const publicPost = mockroblog.getPublicTimeline()
 //Event Listeners 
 btn.addEventListener('click', () => {
     menu.classList.toggle("hidden")
 })
+
+publicPost.forEach(obj => {
+    textPost.innerHTML += `
+    <h1>
+    Time: ${obj.timestamp}
+    </h1>
+    <p>${obj.text}</p>
+    <br>
+    `
+})
+const c = 2
+console.log(`feojfpwejgpwjgpewojgpowe ${c}`);
 
