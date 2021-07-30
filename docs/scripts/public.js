a49ddf5f9d81195c7b7a46bb3680d2a1c9a1459c
Mike Nguyen <mikekietnguyen@csu.fullerton.edu>
2021-07-30 14:45:27 -0700

    added dynamic generating text boxes

--- docs/scripts/public.js
+++ docs/scripts/public.js
@@ -1,11 +1,105 @@
 /*
 Initialize all components from HTML
 */
+import * as mockroblog from './mockroblog.js'
+
+const textPost = document.querySelector(".publicPostContainer") 
+
 const btn = document.querySelector(".mobile-menu-button")
 const menu = document.querySelector(".mobile-menu")
-//Event Listeners 
+const addFollower = mockroblog.addFollower()
+const publicPost = mockroblog.getPublicTimeline()
+//Event Listeners
 
 btn.addEventListener('click', () => {
     console.log("Hello");
-    menu.classList.toggle("hidden")
+    menu.classList.toggle("hidden");
+})
+
+publicPost.forEach(obj => {
+    textPost.innerHTML += `
+    <div class="userName pt-5 w-40 px-3 font-bold text-blue-700" >
+    </p>Full Name</p>
+    </div>
+
+    <!--follow button-->
+    <div class = "container m-1 w-40 h-10 pl-2 pt-2 pb-2">
+        <button class = "followtbtn w-20 h-5 text-white font-bold tracking-wider rounded bg-blue-500 active:bg-blue-700">
+        follow
+        </button> 
+    </div>
+
+    <!--unfollow button-->
+    <div class = "container m-1 w-40 h-10 pl-2 pt-2 pb-2">
+        <button class = "unfollowbtn w-20 h-5 text-white font-bold tracking-wider rounded bg-blue-500 active:bg-blue-700">
+        unfollow
+        </button> 
+    </div>
+    
+        <div class="timeline-container"> </div>
+        <div class="timeline-pointer" aria-hidden="true">
+            <div class="postContainer bg-white p-6 rounded-md shadow-md">
+                <span
+                <span class="font-bold text-indigo-600 text-sm tracking-wide"> 
+                ${obj.timestamp}</span>
+                <p class="pt-1">
+                    ${obj.text}
+                </p>
+                <br>
+            </div>
+        </div>
+
+        <!--Like/unlike feature-->
+        <div class="container pt-2 pl-2 flex space-x-2">
+            <div class="like active:bg-green-300">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
+                    fill="currentColor">
+                    <path
+                        d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
+                </svg>
+            </div>
+
+            <div class="unlike active:bg-red-300">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
+                    fill="currentColor">
+                    <path
+                        d="M18 9.5a1.5 1.5 0 11-3 0v-6a1.5 1.5 0 013 0v6zM14 9.667v-5.43a2 2 0 00-1.105-1.79l-.05-.025A4 4 0 0011.055 2H5.64a2 2 0 00-1.962 1.608l-1.2 6A2 2 0 004.44 12H8v4a2 2 0 002 2 1 1 0 001-1v-.667a4 4 0 01.8-2.4l1.4-1.866a4 4 0 00.8-2.4z" />
+                </svg>
+            </div>
+        </div>
+    `
+})
+
+const follow = document.querySelector(".followtbtn")
+const unfollow = document.querySelector(".unfollowbtn")
+
+unfollow.classList.toggle("hidden");
+
+follow.addEventListener('click', () => {
+
+    follow.classList.toggle("hidden");
+    console.log("hi")
+
+    if (unfollow.classList.toggle("hidden")){
+
+        unfollow.classList.toggle("visible");
+        console.log("hi")
+    }
+    else{
+        alert("error");
+        console.log("hi")
+    }
+})
+
+unfollow.addEventListener('click', () => {
+
+    unfollow.classList.toggle("hidden");
+
+    if (follow.classList.toggle("hidden")){
+
+        follow.classList.toggle("visible");
+    }
+    else{
+        alert("error");
+    }
 })
