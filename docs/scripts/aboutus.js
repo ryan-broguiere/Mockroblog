2448fbcb7892ff1a13f15b7e24d7891f1c71b01e
ryan <ryanbroguiere123@gmail.com>
2021-07-31 12:04:09 -0700

    need to link wireframe and public

--- docs/scripts/aboutus.js
+++ docs/scripts/aboutus.js
@@ -6,18 +6,40 @@ Initialize all components from HTML
 const btn = document.querySelector(".mobile-menu-button")
 const menu = document.querySelector(".mobile-menu")
 const signUpbtn = document.querySelector('.tempbtn')
-
+const logoutNav = document.querySelector(".logout-nav")
 
 //Event Listeners 
 btn.addEventListener('click', () => {
+    console.log("mobile")
     menu.classList.toggle("hidden")
 })
 
-if(localStorage.getItem('loggedin') === 'true'){
+
+
+if(localStorage.getItem('loggedin') === 'true')
+{
     signUpbtn.textContent = 'Log Out'
-    
-    
+    signUpbtn.style.backgroundColor = "red"
+    logoutNav.classList.toggle("hidden")
+    signUpbtn.addEventListener('click', () => {
+        localStorage.clear()
+        alert("Successfully logged out.")
+        location.href = "index.html"
+    })    
 }
+else
+{
+    signUpbtn.addEventListener('click', () => {
+        location.href = "index.html"
+    }) 
+}
+
+logoutNav.addEventListener('click', () => {
+    localStorage.clear()
+    alert("Successfully logged out.")
+    location.href = "index.html"
+})
+
 
 
 
