23c2a267d141d632cc3ab2b94f8aa492b12318dc
ryan <ryanbroguiere123@gmail.com>
2021-07-30 21:17:56 -0700

    needs fixing

--- docs/scripts/user.js
+++ docs/scripts/user.js
@@ -5,24 +5,37 @@ Initialize all components from HTML
 const btn = document.querySelector(".mobile-menu-button")
 const menu = document.querySelector(".mobile-menu")
 const myPost = document.querySelector(".postContainer")
+const displayUser = document.querySelector(".displayUsername")
+const signUpbtn = document.querySelector('.tempbtn')
 
+
+// if(localStorage.getItem('loggedin') === 'true'){
+//     signUpbtn.textContent = 'Sign Out'
+// }
 
 /*
 Extracting object from local storage and parsing it JSON
 */
 const account = JSON.parse(localStorage.getItem('profile'))
 
+if(localStorage.getItem('loggedin') === 'true'){
+    signUpbtn.textContent = 'Sign Out'
+}
+
 /*
 Api variables
 */
 const userPost = mockroblog.getUserTimeline(account.username)
 
+console.log(`${account.username}`)
+displayUser.textContent = "hello"
 
 //Event Listeners 
 btn.addEventListener('click', () => {
     console.log("Hello");
     menu.classList.toggle("hidden")
 })
+
 
 userPost.forEach(obj => {
     myPost.innerHTML += `
